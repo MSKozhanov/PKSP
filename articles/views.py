5f3a99bf7dba4af8108944cbfca2df6fabaa6412
MSKozhanov <kozhanov312@gmail.com>
2023-09-11 18:43:12 +0300

    lab3 done

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+
+# from django.shortcuts import render
+
+# def hello2(request):
+#     return render(request, 'articles/archive.html',)
 
 from django.shortcuts import render
+from .models import Article  # Импортируем модель Article из вашего приложения
 
 def hello2(request):
-    return render(request, 'articles/static_handler2.html')
+    articles = Article.objects.all()
+    
+    context = {'articles': articles}
+
+    return render(request, 'articles/archive.html', context)
